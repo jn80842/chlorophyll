69d57dc3592cd2456ccdf373224b56ff25649eaf
mangpo <mangpo@eecs.berkeley.edu>
2015-03-19 15:00:25 -0700

    clean up

--- src/partitioner.rkt
+++ src/partitioner.rkt
@@ -121,40 +121,6 @@
     (define best-sol #f)
     
     (define (update-global-sol)
-      ;; Unify solutions symbolically. Use this when solve function by function
-      #|(define unified-hash (make-hash))
-      (define concrete-to-sym (make-hash))
-      
-      (for ([mapping (solution->list global-sol)])
-        (let ([key (car mapping)]
-              [val (evaluate (cdr mapping) best-sol)])
-          (hash-set! concrete-to-sym val key)
-          (hash-set! unified-hash key val)))
-      
-      (for ([mapping (solution->list best-sol)])
-        (let ([key (car mapping)]
-              [val (evaluate (cdr mapping) best-sol)])
-          (when (not (hash-has-key? unified-hash key))
-            (hash-set! concrete-to-sym val key)
-            (hash-set! unified-hash key val))))
-      
-      (if (equal? (get-field name func-ast) "main")
-          (set-global-sol (sat (make-immutable-hash (hash->list unified-hash))))
-          (let ([global-hash (make-hash)])
-            (hash-for-each unified-hash 
-                           (lambda (key val) 
-                             (hash-set! global-hash key (hash-ref concrete-to-sym val))))
-            (set-global-sol (sat (make-immutable-hash (hash->list global-hash))))))   
-
-      ;; Unify solutions concretely. Don't use this
-      (for ([mapping (solution->list best-sol)])
-        (let ([key (car mapping)]
-              [val (cdr mapping)])
-          (when (not (hash-has-key? partial-hash key))
-            (hash-set! partial-hash key val))))
-        
-      (set-global-sol (sat (make-immutable-hash (hash->list partial-hash)))|#
-      
       ;; Use this when solve the entire program at once.
       (set-global-sol best-sol)
       (cores-evaluate cores)
@@ -174,60 +140,6 @@
       )
     
     (define t 0)
-      
-                
-    ;; (define (inner-loop)
-    ;;   (pretty-display (format "num-msg <= ~a" middle))
-    ;;   #|(if middle
-    ;;       (solve-with-sol (assert (<= num-msg middle)) global-sol)
-    ;;       (solve-with-sol (assert #t) global-sol))|#
-    ;;   (set! t (current-seconds))
-    ;;   (if middle
-    ;;       (solve (assert (<= num-msg middle)))
-    ;;       (solve (assert #t)))
-    ;;   (pretty-display `(solve-time ,(- (current-seconds) t)))
-    ;;   (set! upperbound (evaluate num-msg))
-    ;;   (set! middle (floor (/ (+ lowerbound upperbound) 2)))
-      
-    ;;   (set! best-sol (current-solution))
-      
-    ;;   ;; display
-    ;;   (pretty-display (format "# messages = ~a" (evaluate num-msg)))
-    ;;   (pretty-display (format "# cores = ~a" (evaluate num-cores)))
-      
-    ;;   ;; recored best-so-far solution
-    ;;   (let ([saved-sol global-sol])
-    ;;     (set-global-sol best-sol)
-    ;;     (with-output-to-file #:exists 'truncate (format "~a/~a.bestsofar" outdir name)
-    ;;       (lambda () 
-    ;;         (pretty-display (format "# messages = ~a" (evaluate num-msg)))
-    ;;         (send my-ast accept concise-printer)
-    ;;         (display-cores cores)))
-    ;;     (set-global-sol saved-sol))
-      
-    ;;   (if (< lowerbound upperbound)
-    ;;       (inner-loop)
-    ;;       (update-global-sol))
-    ;;   )
-    
-    ;; (define (outter-loop)
-    ;;   (with-handlers* 
-    ;;    ([exn:fail? (lambda (e) 
-    ;;                  (pretty-display `(solve-time ,(- (current-seconds) t)))
-    ;;                  (if (and upperbound (or (equal? (exn-message e)
-    ;;                                                  "solve: no satisfying execution found")
-    ;;                                          (equal? (exn-message e)
-    ;;                                                  "assert: failed")))
-    ;;                      (begin
-    ;;                        (set! lowerbound (add1 middle))
-    ;;                        (set! middle (floor (/ (+ lowerbound upperbound) 2)))
-    ;;                        (if (< lowerbound upperbound)
-    ;;                            (outter-loop)
-    ;;                            (update-global-sol)))
-    ;;                      (begin
-    ;;                        (pretty-display e)
-    ;;                        (raise e))))])
-    ;;    (inner-loop)))
 
     (define (inner-loop)
       (set! t (current-seconds))
